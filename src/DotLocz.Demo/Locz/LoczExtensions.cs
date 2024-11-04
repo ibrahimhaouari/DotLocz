d2000cc5012c9817dcf7bbd2714b55ed54b706c2
Ibrahim Haouari <ibrahim.haouari@outlook.fr>
2024-11-04 19:24:24 +0100

    Refactor localization extension methods for improved clarity and consistency

--- src/DotLocz.Demo/Locz/LoczExtensions.cs
+++ src/DotLocz.Demo/Locz/LoczExtensions.cs
@@ -5,5 +5,8 @@ namespace DotLocz.Demo.Locz;
 public static class LoczExtensions
 {
     public static string Get(this IStringLocalizer localizer, Enum key, params object[] args) =>
-        localizer[key.ToString(), args];
+        localizer.Get(key.ToString(), args);
+
+    public static string Get(this IStringLocalizer localizer, string key, params object[] args) =>
+        localizer[key, args];
 }
--- src/DotLocz/LoczService.cs
+++ src/DotLocz/LoczService.cs
@@ -159,7 +159,10 @@ public sealed class LoczService
         public static class LoczExtensions
         {
             public static string Get(this IStringLocalizer localizer, Enum key, params object[] args) =>
-                localizer[key.ToString(), args];
+                localizer.Get(key.ToString(), args);
+
+            public static string Get(this IStringLocalizer localizer, string key, params object[] args) =>
+                localizer[key, args];
         }
         """;
 
